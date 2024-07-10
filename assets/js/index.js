95a8b5646b2be1475766fea48bf32ab4d9d646c9
Chris Phelps <c.j.phelps89@gmail.com>
2024-07-09 22:37:09 -0400

    added constructor to create a card element for each book that is returned as a result

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,62 @@
 // Function to fetch data from the Open Library API
 function fetchBooks(searchQuery, searchCriteria) {
-  fetch(
-    `https://openlibrary.org/search.json?${searchCriteria.toLowerCase()}=${searchQuery}`
-  )
+  fetch(`https://openlibrary.org/search.json?${searchCriteria.toLowerCase()}=${searchQuery}`)
     .then((response) => response.json())
     .then((data) => displaySearchResults(data))
     .catch((error) => console.error("Fetch error:", error));
 }
 
+function displaySearchResults({ docs }) {
+  // empty out any older search results to display the latest search
+  $("#append-searches").empty();
+  for (let books of docs) {
+    const strTitle = books.title;
+    let strAuthor;
+    if (Array.isArray(books.author_name)) {
+      strAuthor = books.author_name[0];
+    } else {
+      strAuthor = books.author_name;
+    }
+    const numPages = books.number_of_pages_median;
+    const firstPub = books.first_publish_year;
+    const ratings = books.ratings_average;
+    // store this incase we want to create a more elaborate design later
+    const coverCode = books.cover_i;
+
+    // CARD: construct our card element based off of bulma documentation
+    const card = $("<div>").addClass("card");
+    // HEAD: give our card a header and assign the bulma class
+    const header = $("<header>").addClass("card-header");
+    // append our strTitle to the header
+    $("<p>").addClass("card-header-title").text(strTitle).appendTo(header);
+
+    // BODY: construct our card body based on bulma documentation
+    const content = $("<div>").addClass("card-content");
+    const contentInner = $("<div>").addClass("content");
+    // if the following values exist for the data set then append them to the inner content as paragraphs
+    if (strAuthor) $("<p>").text(`Author: ${strAuthor}`).appendTo(contentInner);
+    if (firstPub) $("<p>").text(`First Published: ${firstPub}`).appendTo(contentInner);
+    if (numPages) $("<p>").text(`Number of Pages: ${numPages}`).appendTo(contentInner);
+    if (ratings) $("<p>").text(`Rating: ${ratings}`).appendTo(contentInner);
+    // append all the inner content to the card main content
+    content.append(contentInner);
+
+    // FOOTER: construct our footer based on bulma documentation
+    const footer = $("<footer>").addClass("card-footer");
+    // add a button to the bottom of the content that allows for the book to be saved into local storage
+    $("<a>").attr({ href: "#", id: strTitle }).addClass("card-footer-item").text("Save").appendTo(footer);
+
+    // append our created elements to the card element shell
+    card.append(header);
+    card.append(content);
+    card.append(footer);
+    $("#append-searches").append(card);
+  }
+}
+
 // Event listener for the search button click
 document.getElementById("btn-search").addEventListener("click", () => {
-  const searchValue = document
-    .getElementById("search-value")
-    .value.replace(" ", "+");
+  const searchValue = document.getElementById("search-value").value.replace(" ", "+");
   const searchCriteria = document.getElementById("search-criteria").value;
   fetchBooks(searchValue, searchCriteria);
 });
