1d54bed24d640058f306b759b6a718c9d96a9a05
Feleg Teklezghi <felegteklezghi@gmail.com>
2024-07-12 22:04:56 -0400

    save books and display them as array

--- assets/js/library.js
+++ assets/js/library.js
@@ -5,7 +5,9 @@ document.querySelector(".button").addEventListener("click", function () {
 const apiKey = "GEoGrfuYmgUebEklTl8MwWQBlM6O3TWh";
 
 function retrieveRecomendations() {
-  fetch(`https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${apiKey}`)
+  fetch(
+    `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=${apiKey}`
+  )
     .then((response) => response.json())
     .then((data) => displayRecommendations(data))
     .catch((error) => console.error("Fetch error:", error));
@@ -20,8 +22,45 @@ function displayRecommendations({ results }) {
   //   console.log(randNum);
 }
 
+// Function to display selected books on library.html
+function displaySelectedBooks() {
+  const savedBooks = JSON.parse(localStorage.getItem("selectedBooks"));
+  const savedBooksContainer = document.getElementById("selected-books");
+  const displayedTitles = new Set();
+
+  if (savedBooks && Array.isArray(savedBooks)) {
+    savedBooks.forEach((savedBook) => {
+      if (!displayedTitles.has(savedBook.title)) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML = `
+              <div class="card-content">
+                <div class="image-container">
+                  <img src="https://covers.openlibrary.org/b/id/${savedBook.cover}-M.jpg" alt="Cover for ${savedBook.title}">
+                </div>
+                <div class="text-container">
+                  <p>Title: ${savedBook.title}</p>
+                  <p>Author: ${savedBook.author}</p>
+                  <p>Pages: ${savedBook.pages}</p>
+                  <p>Published: ${savedBook.published}</p>
+                  <p>Rating: ${savedBook.rating}</p>
+                </div>
+              </div>
+              `;
+
+        savedBooksContainer.appendChild(card);
+        displayedTitles.add(savedBook.title);
+      }
+    });
+  }
+}
+
+// Call the function to display selected books
+displaySelectedBooks();
+
 $(document).ready(() => {
-//   retrieveRecomendations();
+  //   retrieveRecomendations();
 });
 
 document.querySelector(".button").addEventListener("click", function () {
