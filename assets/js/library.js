1abb08dc19160b7f118093b30df4db068a69abed
Feleg Teklezghi <felegteklezghi@gmail.com>
2024-07-15 18:54:50 -0400

    added delete button to un-save books from library

--- assets/js/library.js
+++ assets/js/library.js
@@ -6,16 +6,27 @@ const apiKey = "GEoGrfuYmgUebEklTl8MwWQBlM6O3TWh";
 
 function displayRecommendations({ results }) {
   const randNum = Math.floor(Math.random() * results.lists.length);
-  $("#recommended").append("<p>").addClass("is-size-4").text(results.lists[randNum].list_name);
+  $("#recommended")
+    .append("<p>")
+    .addClass("is-size-4")
+    .text(results.lists[randNum].list_name);
   for (books of results.lists[randNum].books) {
     const card = $("<div>").addClass("card");
     const header = $("<header>").addClass("card-header");
     $("<p>").addClass("card-header-title").text(books.title).appendTo(header);
     const content = $("<div>").addClass("card-content");
     const contentInner = $("<div>").addClass("content");
-    $(`<img src='${books.book_image}' alt='Cover art for '${books.title}'>`).appendTo(contentInner);
-    $("<p>").text(`Author: ${books.author}`).addClass("author").appendTo(contentInner);
-    $("<p>").text(`Description: ${books.description}`).addClass("author").appendTo(contentInner);
+    $(
+      `<img src='${books.book_image}' alt='Cover art for '${books.title}'>`
+    ).appendTo(contentInner);
+    $("<p>")
+      .text(`Author: ${books.author}`)
+      .addClass("author")
+      .appendTo(contentInner);
+    $("<p>")
+      .text(`Description: ${books.description}`)
+      .addClass("author")
+      .appendTo(contentInner);
     content.append(contentInner);
     card.append(header, content);
     $("#recommended").append(card);
@@ -54,6 +65,7 @@ function displaySelectedBooks() {
                   <p>Pages: ${savedBook.pages}</p>
                   <p>Published: ${savedBook.published}</p>
                   <p>Rating: ${savedBook.rating}</p>
+                  <button class="button is-danger is-outlined delete-button" data-title="${savedBook.title}">Delete</button>
                 </div>
               </div>
               `;
@@ -62,16 +74,36 @@ function displaySelectedBooks() {
         displayedTitles.add(savedBook.title);
       }
     });
+
+    //delete button
+    const deleteButtons = document.querySelectorAll(".delete-button");
+    deleteButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        const title = this.getAttribute("data-title");
+
+        //confirmation before deleting the book
+        const isConfirmed = confirm(
+          `Are you sure you want to delete the book "${title}"?`
+        );
+
+        if (isConfirmed) {
+          let savedBooks = JSON.parse(localStorage.getItem("selectedBooks"));
+          savedBooks = savedBooks.filter((book) => book.title !== title);
+          localStorage.setItem("selectedBooks", JSON.stringify(savedBooks));
+
+          savedBooksContainer.innerHTML = "";
+          displaySelectedBooks();
+        }
+      });
+    });
   }
 }
 
 // Call the function to display selected books
 displaySelectedBooks();
 
-
 $(document).ready(() => {
   retrieveRecomendations();
-
 });
 
 document.querySelector(".button").addEventListener("click", function () {
